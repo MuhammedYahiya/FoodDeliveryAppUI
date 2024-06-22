9ec7de39a653ec44dc5e8d6f1f92db57bd803404
MuhammedYahiya <yahiyahmhd@gmail.com>
2024-06-23 03:32:31 +0530

    live api calling feature implement

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,18 +1,34 @@
-import resList from "../utils/mocData";
 import RestaurantCard from "./RestaurantCard";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import ShimmerUI from "./ShimmerUI";
 
 const Body = () => {
-  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
 
-  return (
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=9.91850&lng=76.25580&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    setListOfRestaurants(
+      json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+
+  return listOfRestaurants.length === 0 ? (
+    <ShimmerUI />
+  ) : (
     <div className="body">
       <div className="filter">
         <button
           className="filter-btn"
           onClick={() => {
             let filteredRes = listOfRestaurants.filter((res) => {
-              return res.info.avgRating > 4.5;
+              return res.info.avgRating > 4;
             });
             setListOfRestaurants(filteredRes);
           }}
---
+++ src/components/ShimmerUI.js
@@ -0,0 +1,16 @@
+const ShimmerUI = () => {
+  return (
+    <div className="shimmer-container">
+      <div className="shimmer-block"></div>
+      <div className="shimmer-block"></div>
+      <div className="shimmer-block"></div>
+      <div className="shimmer-block"></div>
+      <div className="shimmer-block"></div>
+      <div className="shimmer-block"></div>
+      <div className="shimmer-block"></div>
+      <div className="shimmer-block"></div>
+    </div>
+  );
+};
+
+export default ShimmerUI;
