2db3cd5c4fa7cf6959f16e09b692f23272658f4f
MuhammedYahiya <yahiyahmhd@gmail.com>
2024-06-23 23:28:16 +0530

    implement react-router-dom

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,10 @@
 import { LOGO_URL } from "../utils/constants";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
+  const [ButtonRegister, setButtonRegister] = useState("Login");
+
   return (
     <div className="header">
       <div className="logo">
@@ -8,9 +12,19 @@ const Header = () => {
       </div>
       <div className="nav-items">
         <ul>
-          <li>Home</li>
-          <li>About Us</li>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About Us</Link></li>
           <li>Cart</li>
+          <button
+            className="lg-button"
+            onClick={() => {
+              ButtonRegister == "Login"
+                ? setButtonRegister("Logout")
+                : setButtonRegister("Login");
+            }}
+          >
+            {ButtonRegister}
+          </button>
         </ul>
       </div>
     </div>
